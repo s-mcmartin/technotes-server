b21b0c31adecfc250166d2518f67912fad50348c
s-mcmartin <shannoncmcguire@outlook.com>
2023-09-05 18:18:07 -0400

    add allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["https://technotes-w3xe.onrender.com/"];
+const allowedOrigins = [
+  "https://technotes-w3xe.onrender.com",
+  "http://technotes-w3xe.onrender.com",
+  "https://technotes.onrender.com",
+];
 
 module.exports = allowedOrigins;
