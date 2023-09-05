b1c2b3fbf3a215d28411380f786ffacb8689f53a
s-mcmartin <shannoncmcguire@outlook.com>
2023-09-05 17:53:52 -0400

    Ready to deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,3 @@
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
-]
+const allowedOrigins = ["https://technotes-w3xe.onrender.com/"];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
